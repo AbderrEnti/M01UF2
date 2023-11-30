1f741e178db3fe4233ae5eaefec3624518bbc076
Abderr Ahia <abderraman.ahiayouch@enti.cat>
2023-11-30 08:52:26 +0100

    FILE_NAME:SendCheck

--- eftp/client.sh
+++ eftp/client.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
 IP=`ip address | grep inet | head -n3 | tail -n 1| cut -d " " -f 6 | cut -d "/" -f 1`
-SERVER= "localhost "
+SERVER= "localhost"
+TIMEOUT=1
 echo "cliente de EFTP"
 echo "(1)Send "
-echo "EFTP 1.0 " | nc localhost 3333
+echo "EFTP 1.0 " | nc $SERVER 3333
 echo "(2)Listen "
-DATA= `nc -l -p 3333 -w 0`
+DATA= `nc -l -p 3333 -w $TIMEOUT`
 echo $DATA 
 echo "(5)Test & Send"
-if [ " &DATA" != "OK_HEADER"]
-then echo "ERROR 1: BAD HEADER"
+if [ "&DATA" != "OK_HEADER"]
+then 
+	echo "ERROR 1: BAD HEADER"
 	exit 1 
 	fi
 	echo "BOOOM"
 	sleep 1
-	echo "BOOOM" | nc localhost 3333
+	echo "BOOOM" | nc $SERVER 3333
 	echo "(6)Listen"
-	DATA `nc -l -p 3333 -w 0`
+	DATA `nc -l -p 3333 -w $TIMEOUT`
 	echo $DATA
 	echo "(9)Test"
 	if [ "$DATA" != "OK_HANDSHAKE" ]
 	then echo "ERROR 2: BAD HANDSHAKE"
 		exit 2
 	fi
-	FILE_NAME=fary1.txt
-	FILE_MD5=`echo $FILE_NAME | md5sum | cut -d " " -f 1`
 	echo "(10) Send"
+	 FILE_NAME="fary1.txt"
+    FILE_MD5=`echo $FILE_NAME | md5sum | cut -d " " -f 1`
 	sleep 1
-	echo " FILE_NAME $FILE_NAME" | nc $SERVER 3333
+	echo " FILE_NAME $FILE_NAME $FILE_MD5" | nc $SERVER 3333
 	echo "(11)Listen"
-	DATA=`nc -l -p 3333 -w 0`
+	DATA=`nc -l -p 3333 -w $TIMEOUT`
 	echo "(14)Test&Send"
 	if [ $DATA != "OK_FILE_NAME" ]
 	then echo "ERROR 3: BAD FILE NAME PREFIX"
@@ -38,7 +40,7 @@ then echo "ERROR 1: BAD HEADER"
 	sleep 1 
 	cat imgs/fary1.txt | nc $SERRVER 3333
 	echo "(15)Listen"
-	DATA= `nc -l -p 3333 -w 0`
+	DATA= `nc -l -p 3333 -w $TIMEOUT`
 	 if [ "$DATA" != "OK_DATA" ]
 	 then 
 		 echo "ERROR 4: BAD DATA"
@@ -48,7 +50,7 @@ then echo "ERROR 1: BAD HEADER"
 	 FILE_MD5=`cat imgs/$FILE_NAME | md5sum | cut -d " " -f 1`
 	 echo "file_md5 $FILE_MD5" | nc $SERVER 3333
 	 echo "(19)Listen"
-	 DATA=`nc -l -p 3333 -w 0`
+	 DATA=`nc -l -p 3333 -w $TIMEOUT`
 
 echo "FIN"
  exit 0
--- eftp/server.sh
+++ eftp/server.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 CLIENT="10.65.0.61"
+TIMEOUT=1
 echo "sevidor de EFTP"
 echo "(0)Listen"
-DATA=`nc -l -p 3333 -w 0`
+DATA=`nc -l -p 3333 -w $TIMEOUT`
 echo $DATA
 echo "(3)Test & Sed"
-if [ "$DATA" != "EFTP 1.0 " ]
-then  
+if [ "$DATA" != "EFTP 1.0" ]
+then 
  echo "ERROR 1 : BAD HEADER"
 sleep 1 
-echo "KO_HEADER" nc localhost 3333
+echo "KO_HEADER" | nc $CLIENT 3333
        exit 1	
 fi
 echo "OK_HEADER"
 sleep 1
-echo "OK_HEADER" | nc localhost 3333
+echo "OK_HEADER" | nc $CLIENT 3333
 echo "(4) Listen"
-DATA=`nc -l -p 3333 -w 0`
-echo "(7)Test $ Send"
+DATA=`nc -l -p 3333 -w $TIMEOUT`
 echo $DATA
-if $[ "$DATA" != "BOOOM" ]
+echo "(7)Test $ Send"
+if [ "$DATA" != "BOOOM" ]
  then
 	 echo "ERROR 2: BAD HANDSHAKE2"
 	 sleep 1
@@ -30,7 +31,8 @@ fi
 sleep 1
 echo "OK_HANDSHAkE" 
 echo "(8)Listen"
-	DATA= `nc -l -p 3333 -w 0`
+	DATA= `nc -l -p 3333 -w $TIMEOUT`
+	echo $DATA
 echo "(12)Test&Store&Send"
 PREFIX= echo`$DATA | cut -d " " -f 1`
 if [ "$PREFIX" != "FILE_NAME" ]
@@ -40,16 +42,16 @@ then echo "ERROR 3:BAD FILE_NAME PREFIX"
        exit 3
 fi
 
-FILE_NAME=`echo $DATA | cut -d " " -f 2`
+echo "OK_FILE_NAME" | nc $CLIENT 3333
 echo "(13)Listen"
-nc -l -p 3333-w 0 > inbox/$FILE_NAME
+nc -l -p 3333-w $TIMEOUT > inbox/$FILE_NAME
 DATA=`cat inbox/$FILE_NAME`
 echo "(16)Store&Send"
 if [ "$DATA" == "" ]
 then 
 	echo "ERROR 4: EMPTY DATA"
 	sleep 1
-	echo "KO_DATA" | NC $CLIENT 3333
+	echo "KO_DATA" | nc $CLIENT 3333
 fi
 
 echo $DATA > inbox/$FILE_NAME
@@ -58,7 +60,7 @@ echo "OK_DATA" | nc $CLIENT 3333
 exit 0
 
 echo "(17)Listen"
-DATA=`nc -l -p 3333 -w 0`
+DATA=`nc -l -p 3333 -w $TIMEOUT`
 echo "(20)Test&Send"
 
 
